2539a4f05d0add9a5c7b9d2cfe96c50af8a37d23
SVIC92 <victorjosesanchezlaureano@gmail.com>
2026-04-11 21:27:43 -0500

    Primer commit hacia el nuevo repositorio

--- src/main/java/com/GestionInscripcionCursos/controladores/ActividadControlador.java
+++ src/main/java/com/GestionInscripcionCursos/controladores/ActividadControlador.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+//prueba
 @Controller
 @RequestMapping("/actividad")
 public class ActividadControlador {
